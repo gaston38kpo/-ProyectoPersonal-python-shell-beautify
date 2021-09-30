477635999e00b417a8438dd9a908339efc373717
Giacobini Gaston <77559010+gaston38kpo@users.noreply.github.com>
2021-09-30 18:38:37 -0300

    Add files via upload

---
+++ shell_colors.py
@@ -0,0 +1,124 @@
+###############################################################################
+############################### Aditional #####################################
+###############################################################################
+
+end = '\033[0m'       # Text reset (Important!)
+
+bold = '\033[01m'      # Bold text
+disable = '\033[02m'    # Disable..?
+italic = '\033[03m'   # Italic text
+underline = '\033[04m'  # Underline text
+blink = '\033[05m'    # Blink text
+blinkFast = '\033[06m'  # Blink fast text
+reverse = '\033[07m'    # Reverse colors
+occult = '\033[08m'  # Occult text
+strikethrough = '\033[09m'  # Strikethrough text
+
+###############################################################################
+############################# Regular colors ##################################
+###############################################################################
+# Normal
+black = '\033[30m'
+red = '\033[31m'
+green = '\033[32m'
+yellow = '\033[33m'
+blue = '\033[34m'
+purple = '\033[35m'
+cyan = '\033[36m'
+white = '\033[37m'
+
+#  Bold
+black_b = '\033[1;30m'
+red_b = '\033[1;31m'
+green_b = '\033[1;32m'
+yellow_b = '\033[1;33m'
+blue_b = '\033[1;34m'
+purple_b = '\033[1;35m'
+cyan_b = '\033[1;36m'
+white_b = '\033[1;37m'
+
+# Italic
+black_i = '\033[3;30m'
+red_i = '\033[3;31m'
+green_i = '\033[3;32m'
+yellow_i = '\033[3;33m'
+blue_i = '\033[3;34m'
+purple_i = '\033[3;35m'
+cyan_i = '\033[3;36m'
+white_i = '\033[3;37m'
+
+# Underline
+black_u = '\033[4;30m'
+red_u = '\033[4;31m'
+green_u = '\033[4;32m'
+yellow_u = '\033[4;33m'
+blue_u = '\033[4;34m'
+purple_u = '\033[4;35m'
+cyan_u = '\033[4;36m'
+white_u = '\033[4;37m'
+
+###############################################################################
+########################## High intensity colors ##############################
+###############################################################################
+# Normal
+hi_black = '\033[90m'
+hi_red = '\033[91m'
+hi_green = '\033[92m'
+hi_yellow = '\033[93m'
+hi_blue = '\033[94m'
+hi_purple = '\033[95m'
+hi_cyan = '\033[96m'
+hi_white = '\033[97m'
+
+# Bold
+hi_black_b = '\033[1;90m'
+hi_red_b = '\033[1;91m'
+hi_green_b = '\033[1;92m'
+hi_yellow_b = '\033[1;93m'
+hi_blue_b = '\033[1;94m'
+hi_purple_b = '\033[1;95m'
+hi_cyan_b = '\033[1;96m'
+hi_white_b = '\033[1;97m'
+
+# Italic
+hi_black_i = '\033[3;90m'
+hi_red_i = '\033[3;91m'
+hi_green_i = '\033[3;92m'
+hi_yellow_i = '\033[3;93m'
+hi_blue_i = '\033[3;94m'
+hi_purple_i = '\033[3;95m'
+hi_cyan_i = '\033[3;96m'
+hi_white_i = '\033[3;97m'
+
+# Underline
+hi_black_u = '\033[4;90m'
+hi_red_u = '\033[4;91m'
+hi_green_u = '\033[4;92m'
+hi_yellow_u = '\033[4;93m'
+hi_blue_u = '\033[4;94m'
+hi_purple_u = '\033[4;95m'
+hi_cyan_u = '\033[4;96m'
+hi_white_u = '\033[4;97m'
+
+###############################################################################
+################################# Background ##################################
+###############################################################################
+
+bg_black = '\033[40m'
+bg_red = '\033[41m'
+bg_green = '\033[42m'
+bg_yellow = '\033[43m'
+bg_blue = '\033[44m'
+bg_purple = '\033[45m'
+bg_cyan = '\033[46m'
+bg_white = '\033[47m'
+
+# High intensity backgrounds
+bg_black_hi = '\033[100m'
+bg_red_hi = '\033[101m'
+bg_green_hi = '\033[102m'
+bg_yellow_hi = '\033[103m'
+bg_blue_hi = '\033[104m'
+bg_purple_hi = '\033[105m'
+bg_cyan_hi = '\033[106m'
+bg_white_hi = '\033[107m'
